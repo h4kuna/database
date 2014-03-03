9d825639abd066d1629817423e287e3ed353e290
Milan Matějček <milan.matejcek@gmail.com>
2014-03-03 10:05:02 +0100

    Upgrade conndition.

--- mysql/function/null_default.sql
+++ mysql/function/null_default.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE `null_default` (INOUT `_val` text)
 NO SQL
     DETERMINISTIC
 BEGIN
-IF TRIM(_val) = '' OR is_numeric(_val) AND _val = 0 OR _val = '0000-00-00' OR _val = '0000-00-00 00:00:00' THEN
+IF _val = '0000-00-00' OR _val = '0000-00-00 00:00:00' OR TRIM(_val) = '' OR _val = 0 AND is_numeric(_val) THEN
 	SET _val := NULL;
 END IF;
 END;;
