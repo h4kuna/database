ac17c47e82f2501d400e98fc897ba748c0a2b968
Milan Matějček <milan.matejcek@csfd.cz>
2017-01-09 07:11:36 +0100

    Update null default for mysql.

--- mysql/function/null_default.sql
+++ mysql/function/null_default.sql
@@ -4,8 +4,9 @@ CREATE PROCEDURE `null_default` (INOUT `_val` text)
 NO SQL
     DETERMINISTIC
 BEGIN
-IF _val = '0000-00-00' OR _val = '0000-00-00 00:00:00' OR TRIM(_val) = '' OR _val = 0 AND is_numeric(_val) THEN
-	SET _val := NULL;
+IF TRIM(_val) IN ('0000-00-00', '0000-00-00 00:00:00', '-0001-11-30 00:00:00', '-0001-11-30 00:00:00.000000', '') THEN
+-- mysql 5.7+ does not support negative datetime and empty string.
+  SET _val := NULL;
 END IF;
 END;;
 DELIMITER ;
